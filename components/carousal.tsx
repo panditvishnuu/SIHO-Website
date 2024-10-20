dd77e2dce8d4145517877b4d6da1ad666d772372
Vishnu <vishnusharma2k03@gmail.com>
2024-10-20 11:35:28 +0530

    added new carousal images without static image data

--- components/carousal.tsx
+++ components/carousal.tsx
@@ -4,13 +4,6 @@ import React from "react";
 import { images } from "@/constants";
 import { Carousel, Card } from "@/components/ui/apple-cards-carousel";
 
-type CardType = {
-  src: string | StaticImageData;
-  title: string;
-  category: string;
-};
-
-
 export function AppleCardsCarouselDemo() {
   const cards = data.map((card, index) => (
     <Card key={card.src} card={card} index={index} />
@@ -26,13 +19,12 @@ export function AppleCardsCarouselDemo() {
   );
 }
 
-
-const data: CardType[] = [
+const data = [
   {
     category: "Knowledge",
     title:
       "Investing in equities requires a deep understanding of business. Our team of experienced equity analysts with over 15 years of market expertise. We thoroughly analyse financial statements, valuations, sector trends, and competitive landscapes, using technical analysis and macroeconomic insights to guide our decisions.",
-    src: images.carousal1,
+    src: "https://img.freepik.com/free-photo/business-person-futuristic-business-environment_23-2150970197.jpg?t=st=1729404112~exp=1729407712~hmac=bb0635e50325ba28d77ad568d545bfec7ca4f242f502abebf7065835d82447f5&w=740",
   },
   {
     category: "Tech & AI Based Process",
@@ -51,18 +43,18 @@ const data: CardType[] = [
     category: "Analytical",
     title:
       "SIHO employs a rigorous analytical approach to evaluate investment opportunities. This involves collecting and scrutinizing vast amounts of data, including macroeconomic indicators, company fundamentals, and market trends. By leveraging advanced analytical tools and methodologies, SIHO is able to discern patterns and insights that guide investment decisions.",
-    src: images.carousal4,
+    src: "https://img.freepik.com/free-photo/technology-hologram-indoors_23-2151833350.jpg?t=st=1729404143~exp=1729407743~hmac=40b6251c678b09fde21aff12ba877417b1b07506c175f09595b0431c2ccff6d8&w=360",
   },
   {
     category: "Innovative",
     title:
       "At the core of SIHO’s operational philosophy is innovation. The platform integrates the latest advancements in technology, such as artificial intelligence and machine learning, to refine its investment strategies. This innovation extends to the development of proprietary algorithms that analyze market conditions and predict trends, giving SIHO a competitive edge.",
-    src: images.carousal5,
+    src: "https://img.freepik.com/premium-vector/idea-development-businessman-watering-bulb_1050284-531.jpg?w=740",
   },
   {
     category: "Strategic",
     title:
       "SIHO’s investment methodology is rooted in strategic thinking. Every decision is made with a long-term vision in mind, focusing on sustainable growth rather than short-term gains. The strategic framework considers various factors such as market dynamics, company performance, and investor goals.",
-    src: images.carousal6,
+    src: "https://img.freepik.com/premium-photo/businesspeople-gathered-around-table-discussing-project-reviewing-graphs_885831-173215.jpg?w=740",
   },
 ];
